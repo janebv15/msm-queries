48889ee77bfe90cc0c52452798700d776f1d74db
janebv15 <jvaughn0@chicagobooth.edu>
2025-05-08 04:28:04 +0000

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   get("/directors", { :controller => "directors", :action => "index" })
 
-  get("/directors/:the_ID", { :controller => "directors", :action => "show" })
+  get("/directors/:the_id", { :controller => "directors", :action => "show" })
 end
